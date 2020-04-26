ae94ca0f1b2a8e033e58ca02edc5d2f2f644948c
Erick Kill <31928043+erickkill@users.noreply.github.com>
2020-04-26 03:30:43 -0300

    Update script.R

--- script.R
+++ script.R
@@ -1,18 +1,17 @@
-# lendo as bibliotecas
+# lendo as bibliotecas (caso as bibliotecas não estejam instaladas, executar o comando install.packages("nome da lib")
 library('sf')
 library('biscale')
 library('cowplot')
 library('ggplot2')
 
-# setando o diretório
-file.path("..", "shp")
+# setando o diretório (aqui se edita de acordo com o nome da pasta em que o .shp e o script estão inseridos)
+file.path("..", "biscale")
 
 # carregando o shapefile
 shape <- read_sf('SP_final.shp')
 
 # criando as classes e setando na função quantil
 data <- bi_class(shape, x = 'HOMENS', y = 'MULHERES', style = "quantile", dim = 3)
-
 
 # criando o mapa
 mapa <- ggplot() +
@@ -31,8 +30,7 @@ legenda <- bi_legend(pal = "DkBlue",
                     ylab = "Mulheres",
                     size = 10)
 
-
-# combinando mapa com legenda (os números setam as posições da legenda e mapa na plotagem)
+# combinando mapa com a legenda (os números setam as posições da legenda e do mapa na plotagem)
 finalPlot <- ggdraw() +
   draw_plot(mapa, 0, 0, 1, 1) +
   draw_plot(legenda, 0, .1, 0.6, 0.2) 
